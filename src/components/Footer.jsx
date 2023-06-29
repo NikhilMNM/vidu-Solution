a0c83fcac87198231969cf2ad0c819ca013a0440
Nikhil Vema <124234082+NikhilMNM@users.noreply.github.com>
2023-06-29 18:28:04 +0530

    add links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,27 +7,28 @@ function Footer() {
         <div className="container">
             <div className="row">
                 <div className="col-md-3">
-                    <NavLink href="#"><img src="images/logo-2.svg" alt=""/></NavLink><br /><br />
+                    <NavLink to="/"><img src="images/logo-2.svg" alt=""/></NavLink><br /><br />
                     <p>A professional team of industry experts is supported by a workforce that is obsessed with quality and timely delivery.</p>
-                    <NavLink href="#"><h5>ABOUT</h5></NavLink>
-                    <NavLink href="#"><h5>PORTFOLIO</h5></NavLink>
+                    <NavLink to="/About"><h5>ABOUT</h5></NavLink>
+                    <NavLink to="/Portfolio"><h5>PORTFOLIO</h5></NavLink>
                 </div>
                 <div className="col-md-2">
-                    <h5>SERVICES</h5>
-                    <NavLink href="#">Web Application Development</NavLink><br />
-                    <NavLink href="#">Mobile App Development </NavLink><br />
-                    <NavLink href="#">E-commerce</NavLink><br />
-                    <NavLink href="#">Content Management</NavLink><br />
-                    <NavLink href="#">IT Consultaning</NavLink><br />
-                    <NavLink href="#">SAP</NavLink><br />
-                    <NavLink href="#">SAP ARIBA</NavLink><br />
-                    <NavLink href="#">SAP BW</NavLink>
+                    <NavLink to="/Service"><h5>SERVICES</h5></NavLink>
+                    <NavLink to="#">Web Application Development</NavLink><br />
+                    <NavLink to="#">Mobile App Development </NavLink><br />
+                    <NavLink to="#">E-commerce</NavLink><br />
+                    <NavLink to="#">Content Management</NavLink><br />
+                    <NavLink to="#">IT Consultaning</NavLink><br />
+                    <NavLink to="#">SAP</NavLink><br />
+                    <NavLink to="#">SAP ARIBA</NavLink><br />
+                    <NavLink to="#">SAP BW</NavLink>
                 </div>
                 <div className="col-md-3">
-                    <h5>CONTACT</h5>
-                    <NavLink href="#">info@vidusolutions.com</NavLink><br />
-                    <NavLink href="#">(832) 295-9666</NavLink><br /><br />
-                    <NavLink href="#">1500 S Dairy Ashford Rd Suite #320, Houston, TX 77077, United States.</NavLink>
+                    
+                    <NavLink to="/Contact"><h5>CONTACT</h5></NavLink>
+                    <NavLink to="mailto:info@vidusolutions.com">info@vidusolutions.com</NavLink><br />
+                    <NavLink to="tel:8322959666">(832) 295-9666</NavLink><br /><br />
+                    <NavLink to="#">1500 S Dairy Ashford Rd Suite #320, Houston, TX 77077, United States.</NavLink>
                     <br /><br />
                     <i className="bi bi-facebook"></i>
                     <i className="bi bi-youtube"></i>
